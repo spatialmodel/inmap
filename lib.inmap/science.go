6f6a8e0f0f9ade0115e511eabf3ad24f2ce90b11
ctessum <ctessum@gmail.com>
2014-04-02 14:21:52 -0500

    cleaned up science functions

--- lib.inmap/science.go
+++ lib.inmap/science.go
@@ -1,14 +1,5 @@
 package inmap
 
-const (
-	dp    = 1.e-6   // m, particle diameter
-	rhof  = 1.2466  // kg/m3, air density
-	rhop  = 1000.   // kg/m3, density of droplet
-	g     = 9.80665 // m/s2
-	mu    = 1.5e-5  // kg/m/s
-	kappa = 0.4     // von karmon's constant
-)
-
 // Calculate vertical mixing based on Pleim (2007), which is
 // combined local-nonlocal closure scheme, for
 // boundary layer and based on Wilson (2004) for above the boundary layer.
@@ -113,51 +104,20 @@ func (c *Cell) UpwindAdvection(Δt float64) {
 	}
 }
 
-// Partitions organic matter ("gOrg" and "pOrg"), the
-// nitrogen in nitrate ("gNO and pNO"), the nitrogen in ammonia ("gNH" and
-// "pNH) and sulfur ("gS" and "pS") between gaseous and particulate phase
-// based on the spatially explicit partioning present in the baseline data.
-func (c *Cell) ChemicalPartitioning() {
-
-	// Gas/particle partitioning
-	totalOrg := c.Cf[igOrg] + c.Cf[ipOrg]
-	c.Cf[igOrg] = totalOrg * c.OrgPartitioning
-	c.Cf[ipOrg] = totalOrg * (1 - c.OrgPartitioning)
-
-	totalS := c.Cf[igS] + c.Cf[ipS]
-	c.Cf[igS] = totalS * c.SPartitioning
-	c.Cf[ipS] = totalS * (1 - c.SPartitioning)
-
-	totalNO := c.Cf[igNO] + c.Cf[ipNO]
-	c.Cf[igNO] = totalNO * c.NOPartitioning
-	c.Cf[ipNO] = totalNO * (1 - c.NOPartitioning)
-
-	totalNH := c.Cf[igNH] + c.Cf[ipNH]
-	c.Cf[igNH] = totalNH * c.NHPartitioning
-	c.Cf[ipNH] = totalNH * (1 - c.NHPartitioning)
-}
-
 // Calculates the secondary formation of PM2.5 based on the
 // chemical mechanisms from the COBRA model and APEEP models
 // (COBRA user manual appendix A; Muller and Mendelsohn 2006).
 // Changes have been made to adapt the equations from gaussian
 // plume model form to gridded model form.
-// VOC/SOA partitioning is performed using the method above.
+// Partitions organic matter ("gOrg" and "pOrg"), the
+// nitrogen in nitrate ("gNO and pNO"), and the nitrogen in ammonia ("gNH" and
+// "pNH) between gaseous and particulate phase
+// based on the spatially explicit partioning present in the baseline data.
 func (c *Cell) Chemistry(d *InMAPdata) {
-	//totalSparticle := (c.Cf[ipS] + c.Cbackground[ipS]) / mwS    // moles S
-	//totalNHgas := (c.Cf[igNH] + c.Cbackground[igNH]) // μg N
-	//totalNHparticle := (c.Cf[ipNH] + c.Cbackground[ipNH]) / mwN // moles N
-
-	// Rate of SO2 conversion to SO4 (1/s); Muller Table 2
-	//const kS = 0.000002083
-	// Rate of NOx conversion to NO3 (1/s); Muller Table 2, multiplied by COBRA
-	// seasonal coefficient of 0.25 for NH4NO3 formation.
-	//const kNO = 0.000005556 * 0.25
 
 	// All SO4 forms particles, so sulfur particle formation is limited by the
 	// SO2 -> SO4 reaction.
 	ΔS := c.SO2oxidation * c.Cf[igS] * d.Dt
-	//ΔS := kS * c.Cf[igS] * d.Dt
 	c.Cf[igS] -= ΔS
 	c.Cf[ipS] += ΔS
 
@@ -176,26 +136,9 @@ func (c *Cell) Chemistry(d *InMAPdata) {
 	c.Cf[igNO] = totalNO * c.NOPartitioning
 	c.Cf[ipNO] = totalNO * (1 - c.NOPartitioning)
 
-	// Step 2. NH4NO3 formation
-	//if totalNHgas > 0. {
-	//	ΔN := kNO * c.Cf[igNO] * d.Dt
-	//	ΔNO := min(totalNHgas, ΔN)
-	//	ΔNH := min(c.Cf[igNH], ΔNO)
-	//	c.Cf[igNH] -= ΔNH
-	//	c.Cf[ipNH] += ΔNH
-	//	c.Cf[igNO] -= ΔNO
-	//	c.Cf[ipNO] += ΔNO
-	//}
 }
 
-// VOC oxidation flux
-//func (c *Cell) VOCoxidationFlux(d *InMAPdata) {
-//	c.Cf[igOrg] -= c.Ci[igOrg] * d.VOCoxidationRate * d.Dt
-//}
-
-// Calculates Dry deposition using deposition velocities from Muller and
-// Mendelsohn (2006), Hauglustain et al. (1994), Phillips et al. (2004),
-// and and the GOCART aerosol module in WRF/Chem.
+// Calculates particle removal by dry deposition
 func (c *Cell) DryDeposition(d *InMAPdata) {
 	const (
 		vNO2 = 0.01  // m/s; Muller and Mendelsohn Table 2
@@ -222,6 +165,7 @@ func (c *Cell) DryDeposition(d *InMAPdata) {
 	}
 }
 
+// Calculates particle removal by wet deposition
 func (c *Cell) WetDeposition(Δt float64) {
 	particleFrac := 1. - c.ParticleWetDep*Δt
 	SO2Frac := 1. - c.SO2WetDep*Δt
