fd2f858aa1ade6f250bcc5705b5bd3812e744721
Chris Tessum <ctessum@gmail.com>
2016-08-07 17:56:08 -0700

    Changed dynamic grid to default

--- inmap/cmd/run.go
+++ inmap/cmd/run.go
@@ -25,9 +25,9 @@ import (
 )
 
 var (
-	// dynamic specifies whether the simulation should be run with a dynamic
-	// (vs. static) resolution grid.
-	dynamic bool
+	// static specifies whether the simulation should be run with a static
+	// (vs. dynamic) resolution grid.
+	static bool
 
 	// createGrid specifies whether the variable-resolution grid should be
 	// created on-the-fly for static runs rather than reading it from a file.
@@ -39,13 +39,14 @@ func init() {
 	RootCmd.AddCommand(runCmd)
 	runCmd.AddCommand(steadyCmd)
 
-	steadyCmd.PersistentFlags().BoolVarP(&dynamic, "dynamic", "d", false,
-		"Run with a dynamic grid that changes resolution depending on spatial "+
+	steadyCmd.PersistentFlags().BoolVarP(&static, "static", "s", false,
+		"Run with a static grid that is determined before the simulation starts. "+
+			"If false, run with a dynamic grid that changes resolution depending on spatial "+
 			"gradients in population density and concentration.")
 	steadyCmd.PersistentFlags().BoolVar(&createGrid, "creategrid", false,
 		"Create the variable-resolution grid as specified in the configuration file"+
 			" before starting the simulation instead of reading it from a file. "+
-			"If --dynamic is set to true, then this flag will also be automatically set to true.")
+			"If --static is false, then this flag will also be automatically set to false.")
 
 }
 
@@ -63,7 +64,7 @@ var steadyCmd = &cobra.Command{
 	Long: "steady runs InMAP in steady-state mode to calculate annual average " +
 		"concentrations with no temporal variability.",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return labelErr(Run(dynamic, createGrid))
+		return labelErr(Run(!static, createGrid))
 	},
 }
 
