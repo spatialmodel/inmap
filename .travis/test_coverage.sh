be697400f0bc2bc8c82bf168f4e1cc0f59e55ad9
Chris Tessum <ctessum@gmail.com>
2018-09-29 16:09:06 -0700

    travis: Update for modules

--- .travis/test_coverage.sh
+++ .travis/test_coverage.sh
@@ -4,7 +4,7 @@
 # with script found at https://github.com/gopns/gopns/blob/master/test-coverage.sh
 
 echo "mode: set" > acc.out
-returnval=`go test -short -v -coverprofile=profile.out`
+returnval=`GO111MODULE=on go test -short -v -coverprofile=profile.out`
 echo ${returnval}
 if [[ ${returnval} != *FAIL* ]]
 then
@@ -21,7 +21,7 @@ do
 	if ls $Dir/*.go &> /dev/null;
 	then
 		echo $Dir
-		returnval=`go test -short -coverprofile=profile.out $Dir`
+		returnval=`GO111MODULE=on go test -short -coverprofile=profile.out $Dir`
 		echo ${returnval}
 		if [[ ${returnval} != *FAIL* ]]
 		then
